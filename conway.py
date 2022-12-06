cc6b66bafef321047943dce962fcaf057e793ecb
Akinwande Tomisin <tomjames156@gmail.com>
2022-12-06 16:33:08 +0100

    added condition to check living neighbours and update the cells for the next step

--- conway.py
+++ conway.py
@@ -18,7 +18,50 @@ for x in range(width):
 print(next_cells)
 
 while True:
-    print('\n\n\n\n') # separate each step with new lines
-    currentCells = copy.deepcopy(next_cells)
+    try:
+        print('\n\n\n\n') # separate each step with new lines
+        current_cells = copy.deepcopy(next_cells) # copy the cells
 
-    # print current cells
+        # print current cells
+        for y in range(height):
+            for x in range(width):
+                print(current_cells[x][y], end='')
+            print()
+        
+        # Calculate the next step's cells based on current step's cells:
+        for x in range(width):
+            for y in range(height):
+                left_coord = (x - 1) % width # the modulus operator makes sure the coord is with the width and height range always
+                right_coord = (x + 1) % width
+                above_coord = (y + 1) % height
+                below_coord = (y - 1) % height
+
+                num_of_living_neighbours = 0
+                # Use mutliple if statements to check the living status of the cells
+                if (current_cells[left_coord][y] == '#'):
+                    num_of_living_neighbours += 1 # cell to the left is alive
+                if (current_cells[right_coord][y] == '#'):
+                    num_of_living_neighbours += 1 # cell to the right is alive
+                if (current_cells[x][above_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell above is alive
+                if (current_cells[x][below_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell below is alive
+                if (current_cells[right_coord][above_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell top diagonal to the right is alive
+                if (current_cells[right_coord][below_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell bottom diagonal to the right is alive
+                if (current_cells[left_coord][above_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell top diagonal to the left is alive
+                if (current_cells[left_coord][below_coord] == '#'):
+                    num_of_living_neighbours += 1 # cell bottom diagonal to the left is alive
+
+                if(current_cells[x][y] == '#' and (num_of_living_neighbours == 2 or num_of_living_neighbours == 3)):
+                    next_cells[x][y] = '#'
+                elif(current_cells[x][y] == ' ' and num_of_living_neighbours == 3):
+                    next_cells[x][y] = '#'
+                else:
+                    next_cells[x][y] = ' '
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print('Done for neow')
+        break
